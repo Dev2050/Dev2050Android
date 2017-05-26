b0c3310ff79838411deb89019298862eb85d8eaa
Dev2050 <yebcha2050@hotmail.com>
2017-05-26 13:42:32 +0300

    Update WoltMainActivity.java

--- WoltMainActivity.java
+++ WoltMainActivity.java
@@ -8,11 +8,9 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -81,7 +79,6 @@ private ListView lv;
                         }
                         String openingTime = jsonInsider.getString("value");
                         woltProperties.setOpeningTime(openingTime);
-
                     }
                 }
             }catch (JSONException e){
